0c3265df4d283f202c2b575d904f61fbb7db3866
Exfrom <dscelis@soy.sena.edu.co>
2023-07-24 11:09:35 -0500

    creacion tablas base de datos

--- disqueradc/app2/models.py
+++ disqueradc/app2/models.py
@@ -1,3 +1,13 @@
 from django.db import models
-
 # Create your models here.
+class Disquera(models.Model):
+    id=models.AutoField(primary_key=True)
+    nitdisquera=models.CharField(max_length=25)
+    nombredisquera=models.CharField(max_length=100)
+    telefonodisquera=models.CharField(max_length=15)
+    direcciondisquera=models.CharField(max_length=100)
+    estadodisquera=models.BooleanField()
+    
+class Artista(models.Model):
+    id=models.AutoField(primary_key=True)
+    iddisquerafk=models.ForeignKey(Disquera,on_delete=models.CASCADE)
