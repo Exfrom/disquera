4a31584af111db754b589c9a9fb85e1f4dd197ee
Exfrom <dscelis@soy.sena.edu.co>
2023-07-29 07:48:44 -0500

    secreausuario

--- disqueradc/app2/admin.py
+++ disqueradc/app2/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Disquera
+from .models import Artista
 # Register your models here.
+admin.site.register(Disquera)
+admin.site.register(Artista)
